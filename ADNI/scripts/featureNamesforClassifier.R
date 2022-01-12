45dd614ef3cfa117d3aa55a977f206f799a1bc96
meemansasood <41365292+meemansasood@users.noreply.github.com>
2022-01-12 16:53:44 +0100

    Delete ADNI directory

--- ADNI/scripts/featureNamesforClassifier.R
+++
@@ -1,7 +0,0 @@
-data_all = readRDS("data/data_out/data_all_imp_dig.rds")
-
-featureNames = unlist(lapply(data_all, colnames))
-
-featureNames = setdiff(featureNames, "SUBJID")
-featureNames = as.data.frame(featureNames)
-write.csv(featureNames, "featureNamesADNIforClassifier.csv")
